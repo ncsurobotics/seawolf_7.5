9bab89f763f3c394824d83ba7d48ed1825ae4b57
FalconWarriorr <niall.p.mullane@gmail.com>
2019-10-19 13:48:19 -0400

    pid loops in place for holding orientation and depth

--- src/mission_control/scripts/submerge.py
+++ src/mission_control/scripts/submerge.py
@@ -1,13 +1,14 @@
+#! /usr/bin/env python
 #ROS imports
 import rospy
 from geometry_msgs.msg import Vector3
 from serial.msg import ThrusterData
 from serial.msg import ThrusterCmd
-from geometry_msgs inmport Twist
-from std_msgs import Float64
+from geometry_msgs.msg import Twist
+from std_msgs.msg import Float64
 import numpy as np
 
-depth_pid = [0, 0, 0]
+depth_pid = [1, 0, 0]
 rpy_pid = [0, 0, 0]
 
 last_depth_error = 0
@@ -22,15 +23,17 @@ rpy_error_derivative = [0,0,0]
 thruster_vals = []
 depth = 0
 def thruster_cb(data):
+  global thruster_vals, depth
   thruster_vals = data.thruster_values 
   depth = data.depth
 
 #Define callback function for setting depth target
-depth_target = 0
-def depth_target_cb(data):
-  if not data.data == depth_target:
+target_depth = 0
+def target_depth_cb(data):
+  global target_depth, depth_error_integral
+  if not data.data == target_depth:
     depth_error_integral = 0
-  depth_target = data.data
+  target_depth = data.data
 
 #Define callback function for imu data
 rpy = np.array([0,0,0])
@@ -51,15 +54,14 @@ rospy.init_node("submerge_mission")
 
 #ROS Subscribers
 thrusterSub = rospy.Subscriber("thruster_sub", ThrusterData, thruster_cb)
-targetDepthSub = rospy.Subscriber("depth_target", Float64, depth_target_cb)
+targetDepthSub = rospy.Subscriber("target_depth", Float64, target_depth_cb)
 imuSub = rospy.Subscriber("imu_sub", Vector3, imu_cb)
 targetOrientationSub = rospy.Subscriber("orientation_target", Vector3, orientation_target_cb)
 
 #ROS Publisher
-thrusterPub = rospy.Publisher("thruster_pub", ThrusterCmd, queue_size=3)
+thrusterPub = rospy.Publisher("thruster_pub", Twist, queue_size=3)
 
 #Called to tell subscribers to constantly look for msgs
-rospy.spin()
 
 loop_rate = rospy.Rate(10) # 10 Hz
 last_time = rospy.Time.now().to_sec()
@@ -82,11 +84,15 @@ while not rospy.is_shutdown():
   rpy_error_integral += rpy_error*dt
   rpy_error_derivative = (rpy_error - last_rpy_error) / dt
 
-  depth_output = depth_pid[0]*depth_error + depth_pid[1]*depth_error_integral + depth_pid[2]*depth_error_derivative
+  z_output = depth_pid[0]*depth_error + depth_pid[1]*depth_error_integral + depth_pid[2]*depth_error_derivative
   rpy_output = rpy_pid[0]*rpy_error + rpy_pid[1]*rpy_error_integral + rpy_pid[2]*rpy_error_derivative
 
-  thrusterMsg.linear = [0, 0, output]
-  thrusterMsg.angular = rpy_output
+  thrusterMsg.linear.x = 0
+  thrusterMsg.linear.y = 0
+  thrusterMsg.linear.y = z_output
+  thrusterMsg.angular.x = rpy_output[0]
+  thrusterMsg.angular.y = rpy_output[1]
+  thrusterMsg.angular.z = rpy_output[2]
   thrusterPub.publish(thrusterMsg)
 
   last_depth_error = depth_error
